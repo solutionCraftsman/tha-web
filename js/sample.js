bee9455af0eab8909ac19d58fe8c7a4754c54205
Ayodele <ayodele.glow@gmail.com>
2021-03-08 20:43:28 +0100

    class portfolio 99%?

---
+++ js/sample.js
@@ -0,0 +1,16 @@
+
+console.log("hello World")
+
+var foo = [45, 32, 25]
+
+console.log(foo)
+
+var bar = foo
+
+bar[0] = 200
+
+console.log(bar[0])
+console.log(bar)
+console.log(foo[0])
+console.log(foo)
+
