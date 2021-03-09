0835e8bd4bc8b60597bd22404be7ff0414296ada
Ayodele <ayodele.glow@gmail.com>
2021-03-09 15:15:25 +0100

    21-03-09

--- exercises/javascript-exercises-part-1-21-03-09/one/one.js
+++ exercises/javascript-exercises-part-1-21-03-09/one/one.js
@@ -3,6 +3,7 @@
 // true if one of the number is 50 or if their sum is 50.
 
 function checkNumbers(firstNumber, secondNumber) {
+
     return (
         firstNumber === 50 ||
         secondNumber === 50 ||
@@ -17,3 +18,4 @@ console.log(checkNumbers(25, 25))
 console.log(checkNumbers(20, 30))
 console.log(checkNumbers(1, 1))
 console.log(checkNumbers(-50, -50))
+
